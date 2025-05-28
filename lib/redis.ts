045d34832eba10cddab2319489111b47613ce777
Aditya Singh <adityasingh40675@gmail.com>
2025-05-28 19:41:37 +0530

    refactor: deploy local redis instance

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,5 +1,8 @@
 import { Redis } from 'ioredis';
 
-export const redis = new Redis(process.env.REDIS_URL!, {
-    maxRetriesPerRequest: null,
+export const redis = new Redis({
+  host: process.env.REDIS_URL || "redis://localhost:6380",
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6380,
+  password: process.env.REDIS_PASSWORD || "",
+  maxRetriesPerRequest: 0
 });
